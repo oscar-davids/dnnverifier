653c7006fc7dec9ef18aea748a9097292de246d8
oscar-davids <oscar_davids@outlook.com>
2020-11-30 21:16:57 +0800

    make packet check script

--- gendataset.py
+++ gendataset.py
@@ -1,9 +1,12 @@
+import os
 import cv2
 import argparse
 import datetime
 import csv
 import glob
 import numpy as np
+import random
+
 import pandas as pd
 
 if __name__ == "__main__":
@@ -12,6 +15,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
     testdir = args.dir
 
+    vprofiles = ["P720p60fps16x9", "P720p30fps16x9", "P720p25fps16x9", "P720p30fps4x3", "P576p30fps16x9","P576p25fps16x9",
+                     "P360p30fps16x9", "P360p25fps16x9", "P360p30fps4x3", "P240p30fps16x9", "P240p25fps16x9", "P240p30fps4x3",
+                     "P144p30fps16x9"]
+    vprofilenum = len(vprofiles)
+    devmodes = ["nv","sw"]
+
     fileset = [file for file in glob.glob(testdir + "**/*", recursive=True)]
     count = len(fileset) - 1
     binit = False
@@ -19,7 +28,7 @@ if __name__ == "__main__":
     outcsv = "testlist" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".csv"
     fileout = open(outcsv, 'w', newline='')
     wr = csv.writer(fileout)
-    wr.writerow(['filepath', 'width', 'height', 'fps', 'bitrate', 'framecount', 'indices'])
+    wr.writerow(['filepath', 'width', 'height', 'fps', 'bitrate', 'profile', 'devmode', 'framecount', 'indices'])
     max_samples = 10
 
     for file in fileset:
@@ -33,6 +42,8 @@ if __name__ == "__main__":
             height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             fps = cap.get(cv2.CAP_PROP_FPS)
             bitrate = int(cap.get(cv2.CAP_PROP_BITRATE))
+            vproid = random.choice(vprofiles)
+            dmode = random.choice(devmodes)
             frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
             #generate randomize 10 indices
 
@@ -43,7 +54,7 @@ if __name__ == "__main__":
             strindices += '"'
 
             #write dataset list
-            wr.writerow([frelpath, width, height, fps, bitrate, frame_count, strindices])
+            wr.writerow([frelpath, width, height, fps, bitrate, vproid, dmode, frame_count, strindices])
 
         except Exception as e:
             print(e)
